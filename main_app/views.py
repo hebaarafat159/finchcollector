c0d67e131c55e2c8d91dfc40038ed1461d1e9482
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-13 16:59:05 +0000

    Finch Collector Lab - Part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+from .models import Finch
 # Create your views here.
-finchs = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# finchs = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -13,6 +14,11 @@ def about(request):
     return render(request, 'about.html')
 
 def finchs_index(request):
+    finchs = Finch.objects.all()
     return render(request, 'finchs/index.html',{
         'finchs': finchs
     })
+
+def finchs_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finchs/detail.html', { 'finch':finch})
