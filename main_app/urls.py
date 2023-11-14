732725005fa6f84800bab242e47f4554d820aa93
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-14 11:16:09 +0000

    Finch Collector Lab - Part 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('',views.home, name='home'),
+    path('',views.home, name='home'),
     path('about/',views.about, name='about'),
     path('finchs/',views.finchs_index, name='index'),
     path('finchs/<int:finch_id>/', views.finchs_detail, name='detail'),
+    path('finchs/create/', views.FinchCreate.as_view(), name='finch_create'),
+    path('finchs/<int:pk>/update/', views.FinchUpdate.as_view(), name='finchs_update'),
+    path('finchs/<int:pk>/delete/', views.FinchDelete.as_view(), name='finchs_delete'),
 ]
