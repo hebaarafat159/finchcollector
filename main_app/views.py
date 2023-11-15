9119a8d030f4f6caeffe3dfc944496e1374f24f5
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-15 12:05:52 +0000

    Finch Collector Lab - Part 5

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
 # Create your views here.
 # finchs = [
@@ -23,8 +23,13 @@ def finchs_index(request):
 
 def finchs_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  
   feeding_form = FeedingForm()
-  return render(request, 'finchs/detail.html', { 'finch':finch, 'feeding_form': feeding_form})
+  return render(request, 'finchs/detail.html',{
+    'finch':finch, 'feeding_form': feeding_form,'toys': toys_finch_doesnt_have
+    })
 
 def add_feeding(request, finch_id):
  # create a ModelForm instance using the data in request.POST
@@ -38,10 +43,20 @@ def add_feeding(request, finch_id):
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)
 
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def remove_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
 
 class FinchCreate(CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'color', 'age']
   
 class FinchUpdate(UpdateView):
   model = Finch
@@ -50,3 +65,22 @@ class FinchUpdate(UpdateView):
 class FinchDelete(DeleteView):
   model = Finch
   success_url = '/finchs'
+  
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
